09954b2d3d6732a3d56c22d40f2a5080d0e4ff70
Darío Blanco <dario.blanco@edelight.de>
2013-07-24 18:35:31 +0200

    Set testing credentials for everyone

--- foodninja/settings.py
+++ foodninja/settings.py
@@ -6,6 +6,7 @@ BASEURL = 'http://localhost:5000'
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__).replace('\\', '/'))
 
+# Testing credentials
 FOURSQUARE = {
     'CLIENT_ID': u'NQPFYL4UQQDWXPMXVRJNR5MNC3EZIO2FEEJYTVCJB1G35JNS',
     'CLIENT_SECRET': u'2KWDC3Z04Z0H33PJRGKLTUOY43CLNTRDPATZHF1UL3HQ35QE',
