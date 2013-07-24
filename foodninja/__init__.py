96e73bf551689451b2fbe847f0e0c6b213d85080
Darío Blanco <dario.blanco@edelight.de>
2013-07-24 20:26:18 +0200

    Add debug toolbar extension

--- foodninja/__init__.py
+++ foodninja/__init__.py
@@ -1,5 +1,6 @@
 """Webapp package initializing the global app object"""
 from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
 from foursquare import Foursquare
 
 from foodninja.settings import BASEURL, DEBUG, FOURSQUARE, SECRET_KEY
@@ -9,6 +10,10 @@ app.config.update({
     'DEBUG': DEBUG,
     'SECRET_KEY': SECRET_KEY
 })
+
+app.config['DEBUG_TB_PROFILER_ENABLED'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+toolbar = DebugToolbarExtension(app)
 
 # Foursquare client
 fs_client = Foursquare(client_id=FOURSQUARE['CLIENT_ID'],
