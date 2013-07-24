aa6a1c2581e4cf0f9a7c2af42b8b9cfc6ad881c6
Darío Blanco <dario.blanco@edelight.de>
2013-07-24 18:02:28 +0200

    Fix bug when logging the place

--- foodninja/settings.py
+++ foodninja/settings.py
@@ -7,7 +7,7 @@ BASEURL = 'http://localhost:5000'
 BASEDIR = os.path.abspath(os.path.dirname(__file__).replace('\\', '/'))
 
 FOURSQUARE = {
-    'CLIENT_ID': u'',
-    'CLIENT_SECRET': u'',
+    'CLIENT_ID': u'NQPFYL4UQQDWXPMXVRJNR5MNC3EZIO2FEEJYTVCJB1G35JNS',
+    'CLIENT_SECRET': u'2KWDC3Z04Z0H33PJRGKLTUOY43CLNTRDPATZHF1UL3HQ35QE',
     'REDIRECT_URI': '/redirect'
 }
