39bdc995f2c10566133e9ace8825ac1c0fb3cad5
Darío Blanco <dario.blanco@edelight.de>
2013-07-25 11:12:46 +0200

    Add runserver multihost functionality

--- foodninja/runserver.py
+++ foodninja/runserver.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
+import argparse
 
 from foodninja import app
 from foodninja.settings import DEBUG
 
+
 if __name__ == '__main__':
-    app.run(debug=DEBUG)
+    """Runs the foodninja application"""
+    parser = argparse.ArgumentParser(description='Run the foodninja webserver')
+    parser.add_argument("--host", type=str, default='localhost',
+                        help="Webserver host")
+    parser.add_argument("--port", type=int, default=5000,
+                        help="Webserver port")
+    parser.add_argument("--debug", action="store_true", default=DEBUG,
+                        help="Set debug mode")
+    args_dict = vars(parser.parse_args())
+    app.run(host=args_dict['host'], port=args_dict['port'],
+            debug=args_dict['debug'])
